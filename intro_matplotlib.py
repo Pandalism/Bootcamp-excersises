86b92cfab68f022a71ca56ab1f0167fee1b65ce7
Cambridge Spark <tech+cspark_account@cambridgespark.com>
2019-07-25 15:33:01 +0000

    Update repository

---
+++ intro_matplotlib.py
@@ -0,0 +1,30 @@
+"""A set of exercises with matplotlib"""
+
+
+def draw_co2_plot():
+    """
+    Here is some chemistry data
+
+      Time (decade): 0, 1, 2, 3, 4, 5, 6
+      CO2 concentration (ppm): 250, 265, 272, 260, 300, 320, 389
+
+    Create a line graph of CO2 versus time, the line should be a blue dashed
+    line. Add a title and axis titles to the plot.
+    """
+    raise NotImplementedError
+
+
+def draw_equations_plot():
+    """
+    Plot the following lines on the same plot
+
+      y=cos(x) coloured in red with dashed lines
+      y=x^2 coloured in blue with linewidth 3
+      y=exp(-x^2) coloured in black
+
+    Add a legend, title for the x-axis and a title to the curve, the x-axis
+    should range from -4 to 4 (with 50 points) and the y axis should range
+    from 0 to 2. The figure should have a size of 8x6 inches.
+    """
+
+    raise NotImplementedError
