d07602ca517d7d912fd4cfe5510a45878872947f
Pandalism <f.diaz-maroto@hotmail.com>
2020-06-24 23:42:56 +0100

    change to CNN

--- model.py
+++ model.py
@@ -1,18 +1,23 @@
-from keras.layers.core import Dense
+from keras import layers
+from keras import Input
 from keras.models import Sequential
 from keras.utils import np_utils
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
-
+import numpy as np
 
 class Preprocessor(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         return self
 
     def transform(self, X, y=None):
-        X = X.reshape(X.shape[0], 784)
-
+        # convert to 0,1
+        X = X / 255
+        
+        # insert extra dimensions
+        X = np.expand_dims(X, -1)
+        
         if y is None:
             return X
 
@@ -21,12 +26,19 @@ class Preprocessor(BaseEstimator, TransformerMixin):
 
 
 def keras_builder():
-    model = Sequential()
-    model.add(Dense(16, input_shape=(784,), activation="relu"))
-    model.add(Dense(16, input_shape=(784,), activation="relu"))
-    model.add(Dense(16, input_shape=(784,), activation="relu"))
-    model.add(Dense(16, input_shape=(784,), activation="relu"))
-    model.add(Dense(4, activation="softmax"))
+    model = Sequential([
+        layers.ZeroPadding2D((1,1), input_shape=[28,28,1]),
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(128, kernel_size=(3, 3), activation="relu"),
+        layers.Flatten(),
+        layers.Dropout(0.3),
+        layers.Dense(4, activation="softmax")
+    ])
     model.compile(
         optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
     )
@@ -46,6 +58,6 @@ def build_model():
     """
     preprocessor = Preprocessor()
 
-    model = KerasClassifier(build_fn=keras_builder, batch_size=16, epochs=3)
+    model = KerasClassifier(build_fn=keras_builder, batch_size=16, epochs=10)
 
     return Pipeline([("preprocessor", preprocessor), ("model", model)])
