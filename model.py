cb3e3ddfdff5ea8d32b4cd43d9c0a77f7eb00fab
Pandalism <f.diaz-maroto@hotmail.com>
2020-06-25 00:10:40 +0100

    cnn update

--- model.py
+++ model.py
@@ -33,10 +33,12 @@ def keras_builder():
         layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
         layers.MaxPooling2D(pool_size=(2, 2)),
         layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
         layers.MaxPooling2D(pool_size=(2, 2)),
         layers.Conv2D(128, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
         layers.Flatten(),
-        layers.Dropout(0.3),
+        layers.Dropout(0.5),
         layers.Dense(4, activation="softmax")
     ])
     model.compile(
